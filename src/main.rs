63ea9be8baf58bfe98471eedd7bac1de039e1ba2
Isaac Grosof <igrosof@cmu.edu>
2018-02-21 23:49:55 -0500

    Jut circle

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,20 @@ use svg::node::element::Path;
 use svg::node::element::path::Data;
 use svg::node::element::Circle;
 
+const SIZE: u64 = 200;
+const OFFSET: u64 = 20;
+
 fn main() {
-	let data = Data::new()
-		.move_to((10, 10))
-		.line_by((0, 50))
-		.line_by((50, 0))
-		.line_by((0, -50))
-        .close();
-
-	let path = Path::new()
-		.set("fill", "none")
-		.set("stroke", "black")
-		.set("stroke-width", 3)
-		.set("d", data);
-
     let circle = Circle::new()
-        .set("cx", 35)
-        .set("cy", 35)
-        .set("r", 10)
+        .set("cx", SIZE + OFFSET)
+        .set("cy", SIZE + OFFSET)
+        .set("r", SIZE)
         .set("fill", "none")
-        .set("stroke", "blue")
-        .set("stroke-width", 5);
+        .set("stroke", "black")
+        .set("stroke-width", 1);
 
 	let document = Document::new()
-		.set("viewBox", (0, 0, 70, 70))
-		.add(path)
+		.set("viewBox", (0, 0, SIZE * 2 + OFFSET * 2, SIZE * 2 + OFFSET * 2))
         .add(circle);
 
 	svg::save("image.svg", &document).unwrap();
