41a69e9cefa74627523c153566537840c991a67f
Isaac Grosof <igrosof@cmu.edu>
2018-02-21 23:36:09 -0500

    Add circle

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ extern crate svg;
 use svg::Document;
 use svg::node::element::Path;
 use svg::node::element::path::Data;
+use svg::node::element::Circle;
 
 fn main() {
 	let data = Data::new()
 		.move_to((10, 10))
 		.line_by((0, 50))
 		.line_by((50, 0))
-		.line_by((0, -50));
+		.line_by((0, -50))
+        .close();
 
 	let path = Path::new()
 		.set("fill", "none")
@@ -16,9 +18,17 @@ fn main() {
 		.set("stroke-width", 3)
 		.set("d", data);
 
+    let circle = Circle::new()
+        .set("cx", 35)
+        .set("cy", 35)
+        .set("r", 10)
+        .set("stroke", "blue")
+        .set("stroke-width", 5);
+
 	let document = Document::new()
 		.set("viewBox", (0, 0, 70, 70))
-		.add(path);
+		.add(path)
+        .add(circle);
 
 	svg::save("image.svg", &document).unwrap();
 }
